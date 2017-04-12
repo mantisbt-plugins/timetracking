9b59aa929cb5565c812ba556195ffb35ad30862b
epenet <epenet@infinityccs.com>
2017-04-12 21:35:16 +0200

    Add files via upload

--- TimeTracking/pages/show_report.php
+++ TimeTracking/pages/show_report.php
@@ -1,24 +1,5 @@
 <?php 
-/*
-   Copyright 2011 Michael L. Baker
-   Copyright 2017 Erwann Penet
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-
-   Notes: Based on the Time Tracking plugin by Elmar:
-   2005 by Elmar Schumacher - GAMBIT Consulting GmbH
-   http://www.mantisbt.org/forums/viewtopic.php?f=4&t=589	
-*/
+//<a name="bugnotestats" id="bugnotestats" />
 
 require_once( 'core.php' ); 
 require_once( 'core/bug_api.php' );
