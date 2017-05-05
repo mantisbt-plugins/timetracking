eeca04f0da3474cad35888ad5e43682f72d8358b
Carlos Proensa <proensa@gmail.com>
2017-05-05 02:55:29 +0200

    Implement sorting for columns

--- TimeTracking/core/columns.php
+++ TimeTracking/core/columns.php
@@ -4,7 +4,7 @@ namespace TimeTracking;
 class ColumnTotalTime extends \MantisColumn {
 	public $title = 'Total time spent';
 	public $column = 'total_time';
-	public $sortable = false;
+	public $sortable = true;
 
 	public function cache( array $p_bugs ) {
 		if( empty( $p_bugs ) ) {
@@ -31,12 +31,61 @@ class ColumnTotalTime extends \MantisColumn {
 		}
 		plugin_pop_current();
 	}
+
+	public function sortquery( $p_direction ) {
+		/**
+		 * sorting need that the final value exists at query time, so the total times
+		 * must be calculated in the query.
+		 * For each project, access_levels and configured threshold may be different,
+		 * so first a list of projects where the user has permissions is built.
+		 * This list is used to calculate times only for bugs in those projects
+		 */
+		$t_accessible_projects = user_get_all_accessible_projects();
+		$t_valid_projects = array();
+		plugin_push_current( 'TimeTracking' );
+		foreach( $t_accessible_projects as $t_project_id ) {
+			if( user_can_view_project_id( $t_project_id ) ) {
+				$t_valid_projects[] = (int)$t_project_id;
+			}
+		}
+		plugin_pop_current();
+		if( empty( $t_valid_projects ) ) {
+			return;
+		}
+		$t_table = plugin_table( 'data', 'TimeTracking' );
+		$t_map_nulls = ( $p_direction == 'ASC' ) ? DB_MAX_INT : 0;
+		/**
+		 * Sorting requires an outer join, otherwise, the query would not return all bugs!
+		 * We cannot reference bug_table from within the join, so the bug_table is used inside
+		 * the join subquery, to match the allowed projects previously generated
+		 *
+		 * Null values appear when bugs have no time calculation available (by not having records,
+		 * or dont have access). To avoid them appearing as first result in any ordering, nulls
+		 * are mapped (coalesce) to the opposite end of the sort direction.
+		 *
+		 * Notes:
+		 * - we cannot use db_params within the filter join clause (it's not supported)
+		 * - table aliases are used to avoid name conflicts in the final query
+		 */
+		$t_join_clause =
+			'LEFT OUTER JOIN ( SELECT TTB1.id, SUM(time_count) AS tt_total_time'
+			. ' FROM {bug} TTB1'
+			. ' JOIN ' . $t_table . ' TTP1 ON TTB1.id = TTP1.bug_id'
+			. ' WHERE TTB1.project_id IN (' . implode( ',', $t_valid_projects ) . ')'
+			. ' GROUP BY TTB1.id ) TTS1'
+			. ' ON {bug}.id = TTS1.id';
+		$t_order_clause = 'COALESCE( TTS1.tt_total_time, ' . $t_map_nulls . ' ) ' . $p_direction;
+		return array(
+			'join' => $t_join_clause,
+			'order' => $t_order_clause
+		);
+	}
 }
 
 class ColumnMyTime extends \MantisColumn {
 	public $title = 'My time spent';
 	public $column = 'my_time';
-	public $sortable = false;
+	public $sortable = true;
 
 	public function cache( array $p_bugs ) {
 		if( empty( $p_bugs ) ) {
@@ -63,5 +112,40 @@ class ColumnMyTime extends \MantisColumn {
 			}
 		}
 		plugin_pop_current();
+	}
+
+	public function sortquery( $p_direction ) {
+		$t_user_id = auth_get_current_user_id();
+		$t_accessible_projects = user_get_all_accessible_projects();
+		$t_valid_projects = array();
+		plugin_push_current( 'TimeTracking' );
+		foreach( $t_accessible_projects as $t_project_id ) {
+			if( user_can_view_project_id( $t_project_id ) ) {
+				$t_valid_projects[] = (int)$t_project_id;
+			}
+		}
+		plugin_pop_current();
+		if( empty( $t_valid_projects ) ) {
+			return;
+		}
+		$t_table = plugin_table( 'data', 'TimeTracking' );
+		$t_map_nulls = ( $p_direction == 'ASC' ) ? DB_MAX_INT : 0;
+		/**
+		 * This is the same query as total-time, but adding restriction by user id
+		 */
+		$t_join_clause =
+			'LEFT OUTER JOIN ( SELECT TTB2.id, SUM(time_count) AS tt_total_time'
+			. ' FROM {bug} TTB2'
+			. ' JOIN ' . $t_table . ' TTP2 ON TTB2.id = TTP2.bug_id'
+			. ' WHERE TTP2.user_id = ' . $t_user_id
+			. ' AND TTB2.project_id IN (' . implode( ',', $t_valid_projects ) . ')'
+			. ' GROUP BY TTB2.id) TTS2'
+			. ' ON {bug}.id = TTS2.id';
+
+		$t_order_clause = 'COALESCE( TTS2.tt_total_time, ' . $t_map_nulls . ') ' . $p_direction;
+		return array(
+			'join' => $t_join_clause,
+			'order' => $t_order_clause
+		);
 	}
 }
